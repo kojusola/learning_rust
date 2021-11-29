d7fbd4ed28f87eb65c4c0ead8de8aebc41ff8ba3
Fafemi Adeola <adeola5678@gmail.com>
2021-11-29 21:23:20 +0100

    variables

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,47 @@
-use std::io;
-use std::cmp::Ordering;
-use rand::Rng;
+// use std::io;
+// use std::cmp::Ordering;
+// use rand::Rng;
 
 fn main() {
-    println!("Guess the number!");
-    let secret_number = rand::thread_rng().gen_range(1..101);
+//     println!("Guess the number!");
+//     let secret_number = rand::thread_rng().gen_range(1..101);
 
-   loop{
-    println!("Please input your guess.");
+//    loop{
+//     println!("Please input your guess.");
 
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+//     let mut guess = String::new();
+//     io::stdin()
+//         .read_line(&mut guess)
+//         .expect("Failed to read line");
 
-    // let guess: u32 = guess.trim().parse().expect("Please type a number!");
-    let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-    println!("You guessed: {}", guess);
+//     // let guess: u32 = guess.trim().parse().expect("Please type a number!");
+//     let guess: u32 = match guess.trim().parse() {
+//             Ok(num) => num,
+//             Err(_) => continue,
+//         };
+//     println!("You guessed: {}", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => {
-            println!("You win!");
-            break;
-        },
+//     match guess.cmp(&secret_number) {
+//         Ordering::Less => println!("Too small!"),
+//         Ordering::Greater => println!("Too big!"),
+//         Ordering::Equal => {
+//             println!("You win!");
+//             break;
+//         },
+//     }
+//    }
+    // const x = 5;
+    // println!("The value of x is: {}", x);
+    // x = 6;
+    // println!("The value of x is: {}", x);
+    let x = 5;
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {}", x);
     }
-   }
+
+    println!("The value of x is: {}", x);
 }
